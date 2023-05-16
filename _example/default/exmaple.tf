103e24467a0b2520e1a9f061fd40eb9547303898
nileshgadgi <nilesh.gadgi@clouddrove.com>
2023-05-16 15:34:03 +0530

    fix: add azuread auth in backend

--- _example/default/exmaple.tf
+++ _example/default/exmaple.tf
@@ -7,7 +7,7 @@ terraform {
     storage_account_name = "testingdrfit"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc             = true
+    use_azuread_auth     = true
   }
 }
 
